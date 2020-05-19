0a7772f1726c64ed809ddcc3c59265958f97069a
ZacharyMeyer <zacharymeyer1003@gmail.com>
2020-05-18 19:23:32 -0500

    Updating Tabs index.js information.

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,29 @@
 //
 //  Each tab should look like this:
 //    <div class="tab">topic here</div>
+
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+.then((response) => {
+    const dom = document.querySelector('.topics')
+    response.data.topics.forEach(element => {
+        dom.appentChild(createTabs(element))
+    })
+})
+.catch((error) => {
+    console.log('Network Error')
+    console.log(error)
+})
+
+const createTabs = (object) => {
+
+    // Creating Elements
+
+    const tabs1 = document.createElement('div')
+
+    //Adding Classes
+
+    tabs1.innerHTML = object
+
+return tabs1
+}
