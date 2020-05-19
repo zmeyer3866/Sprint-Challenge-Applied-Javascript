79d533905cec8827edbfb01b8996e6b857f421e5
ZacharyMeyer <zacharymeyer1003@gmail.com>
2020-05-18 19:14:41 -0500

    Updating header index.js

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,4 +11,43 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+
+    // Creating Element
+
+    const header = document.createElement ('div')
+    const dates = document.createElement ('span')
+    const title = document.createElement ('h1')
+    const temps = document.createElement ('span')
+
+
+
+    // Creating HTML Structure
+
+    header.appendChild(dates)
+    header.appendChild(title)
+    header.appendChild(temps)
+
+
+
+
+    // Creating classes
+
+    header.classList.add('header')
+    header.classList.add('date')
+    header.classList.add('temp')
+
+
+
+    // Adding content
+
+    dates.innerHTML = 'SMARCH 28, 2019'
+    title.innerHTML = 'Lambda Times'
+    temps.innerHTML = '98°'
+
+    return header
+
+
+}
+
+const container = document.querySelector('.header-container').appendChild(Header())
